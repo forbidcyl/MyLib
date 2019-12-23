05912869c68745a15a18d541547ac13681389844
Jason <2113129591@qq.com>
2019-12-24 00:19:24 +0800

    更新

---
+++ MyCommonLib/ConsoleTest/Basic/Attribution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.Basic
+{
+    /// <summary>
+    /// 特性：
+    ///     1、特性和注释的区别
+    ///     2、声明和使用attribute,attributeUsage
+    ///     3、运行中获取attribute：额外信息 额外操作
+    ///     4、attribute 验证
+    /// </summary>
+    public class Attribution
+    {
+    }
+
+
+    /// <summary>
+    /// 自定义特性:
+    /// </summary>
+    public class CustomeAttribute : Attribute
+    {
+
+    }
+}
---
+++ MyCommonLib/ConsoleTest/Basic/Reflection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.Basic
+{
+
+    /// <summary>
+    /// 反射:
+    ///     1、反射加载dll,读取module、类、方法、特性
+    ///     2、反射创建对象、反射+简单工厂+配置文件；反射：破坏单例、创建泛型
+    ///     3、反射调用实例方法、静态方法、重载方法、调用私有法，调用泛型方法
+    ///     4、反射字段和属性，分别获取值和设置值
+    ///     5、反射的好处与局限性
+    /// </summary>
+    public class Reflection
+    {
+    }
+}
